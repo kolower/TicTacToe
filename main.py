6357f6b518fdaf6b3d2090c6a9b3829e18b1ab73
263232 <263232@student.pwr.edu.pl>
2022-01-18 11:08:30 +0100

    TicTacToe

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-
 import numpy as np
 import itertools
 
